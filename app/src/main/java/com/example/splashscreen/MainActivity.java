7e67972ce4375180e10a6c6b07fbc9d55db08024
abdullahblouch <abdullahblouch143@gmail.com>
2022-12-29 22:01:28 +0500

    done assignment

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void move(View v) {
+        TextView f= findViewById(R.id.forget);
+        TextView r=findViewById(R.id.register);
+        Button btn=(Button) findViewById(R.id.button);
+        Intent i;
+        switch (v.getId()){
+            case R.id.button:
+                i=new Intent(this,menu.class);
+                startActivity(i);
+                break;
+            case R.id.forget:
+                i=new Intent(this,ForgetPass.class);
+                startActivity(i);
+                break;
+            case R.id.register:
+                i=new Intent(this,Register.class);
+                startActivity(i);
+                break;
+            case R.id.forgetBtn:
+                i=new Intent(this,Reset.class);
+                startActivity(i);
+                break;
+        }
 
     }
 }
